5a52f6ccf6d371f23a2d5c54f092dad7e3bec359
p-hantom <917978341@qq.com>
2017-10-15 20:56:27 +0800

    output committed

---
+++ output.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//unfinished:the largest/smallest double
+int main(){
+	int x=2.2;
+	short y=0x7FFF;
+	short z=0xFFFF;
+	unsigned short w=0xFFFF;
+	float u=0.177;
+	
+	printf("%d\n",x);
+	printf("%d\n",y);
+	printf("%d\n",z);
+	printf("%d\n",w);
+	printf("%f\n",u);
+	
+	return 0;
+}
