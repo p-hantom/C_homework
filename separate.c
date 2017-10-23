724b6f04dbd5f3162f7b92bccead0fb45070b9e2
p-hantom <917978341@qq.com>
2017-10-23 16:06:19 +0800

    functioned

--- separate.c
+++ separate.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-int main(){
+void f(){
 	double num;
 	scanf("%lf",&num);
 	int integer=(int )num;
 	double float_part=num-integer;
 	printf("integer part=%d\n float part=%lf",integer,float_part);
+} 
+
+int main(){
+	f();
 	
 	return 0;
 } 
